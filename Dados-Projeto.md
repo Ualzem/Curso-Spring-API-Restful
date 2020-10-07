c932940311b7aa87a19c0d4d02e545b5aea7d3ba
Ualzem <arquivosualzem@gmail.com>
2020-10-07 13:58:36 -0300

    commit inicial

---
+++ Dados-Projeto.md
@@ -0,0 +1,10 @@
+# Projeto API Restful com Spring Boot :1st_place_medal:
+
+### Projeto do Curso JDEV Treinamentos do Professor Alex Egídio
+
+#### Tecnologias Usadas:
+
+- Spring Boot
+- Banco de Dados Postgresql
+- JPA
+
---
+++ inserindo dados no banco 01.sql
@@ -0,0 +1,11 @@
+﻿INSERT INTO public.usuario(
+            id, login, nome, senha)
+    VALUES (10, 'mezlau', 'Ualzem', '1234');
+
+    INSERT INTO public.usuario(
+            id, login, nome, senha)
+    VALUES (11, 'lulu', 'Luísa', '1233674');
+
+    INSERT INTO public.usuario(
+            id, login, nome, senha)
+    VALUES (12, 'ieie', 'Alessandra', '6787');
